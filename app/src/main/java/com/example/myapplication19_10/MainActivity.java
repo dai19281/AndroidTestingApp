8eae52c326005b8f072d2b5eaf5658918952a5a4
Nickos <dai19281@uom.edu.gr>
2020-10-19 19:30:20 +0300

    App(19-10)

--- app/src/main/java/com/example/myapplication19_10/MainActivity.java
+++ app/src/main/java/com/example/myapplication19_10/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.myapplication19_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    private  int nunTimesClicked=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button= findViewById(R.id.button);
+        textView= findViewById(R.id.textView);
+
+        View.OnClickListener listener= new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               nunTimesClicked++;
+               String result = "\n The button clicked " + nunTimesClicked + " times";
+               textView.append(result);
+
+            }
+        };
+
+        button.setOnClickListener(listener);
     }
 }
