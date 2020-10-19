2780d61a07949bbee96ed6e8800706d9745dc0c8
Nickos <dai19281@uom.edu.gr>
2020-10-19 20:10:57 +0300

    App(19-10)

--- app/src/main/java/com/example/myapplication19_10/MainActivity.java
+++ app/src/main/java/com/example/myapplication19_10/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.myapplication19_10;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "Nikos" ;
+    public static final String TEXT_CONTENTS ="MainActivity_textView";
     private Button button;
     private TextView textView;
 
@@ -16,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "TEXT");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,5 +41,60 @@ public class MainActivity extends AppCompatActivity {
         };
 
         button.setOnClickListener(listener);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestard");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "TEXT");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+
+        String key = null;
+        assert savedInstanceState != null;
+        String savedText= savedInstanceState.getString(key: "MainActivity");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(LOG_TAG, "onSaveInstanceState");
+        outState.putString("MainActivity_textView", textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
     }
 }
